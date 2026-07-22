81a68d9290e789f406e63b79c8d57f4d79035e58
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-22 11:16:41 +0545

    models definition

--- src/models/taskModel.ts
+++ src/models/taskModel.ts
@@ -6,11 +6,59 @@ class task extends Model{}
 
 task.init(
     {
-        id:{
-            type:DataTypes
-        }
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
 
-},
+    owner_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "pending",
+    },
+
+    priority: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "medium",
+    },
+
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+
+    reminder_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+
+    reminder_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "pending",
+    },
+
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE,
+    },
 
     {
         sequelize,
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,13 +1,39 @@
 // import { Model } from "sequelize";
-const Model = require('sequelize');
-import { DataTypes } from "sequelize";
-import sequelize from "../config/db";
+// const {Sequelize,DataTypes,Model} = require('sequelize');
+import {Sequelize,DataTypes,Model} from "sequelize"
+// import { DataTypes } from "sequelize";
+// import sequelize from "../config/db";
+const sequelize = require('../config/db');
 class user extends Model{}
 
 user.init(
     {
-        
-
+      id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "user",
+    },
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE,
 },
 
     {
