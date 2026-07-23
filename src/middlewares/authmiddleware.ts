6544be3ef5fdd15cc7eefe0e34566a033ba20da8
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-23 15:47:33 +0545

    authmiddleware setup

---
+++ src/middlewares/authmiddleware.ts
@@ -0,0 +1,18 @@
+import { signInToken,verifyToken } from "../utils/jwt";           
+import { Request,Response,NextFunction } from "express";
+
+function authMiddleware(req:Request,res:Response,next:NextFunction){
+    const authheaders = req.headers.authorization;
+
+    if(!authheaders || !authheaders.startsWith("Bearer ")){
+        return res.status(401).json({error:{message:'no token provided'}});
+    }
+    const token = authheaders.split(" ")[1] as string;
+    try{
+        const decode = verifyToken(token);
+        next();
+    }
+    catch(error){
+        return res.status(403).json({error:{message:'invalid or expires token'}})
+    }
+}
