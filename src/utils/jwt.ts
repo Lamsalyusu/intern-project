7f5d4bb3e5c7b5194938d5a9c885125506ea1c1b
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-23 11:24:06 +0545

    JWT helper function

---
+++ src/utils/jwt.ts
@@ -0,0 +1,14 @@
+// require('dotenv').config();
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+const jwtSecret = process.env.JWT_SECRET as string;
+dotenv.config();
+
+function signInToken(payload:Object):string {
+    return jwt.sign(payload,jwtSecret,{expiresIn:'1d'});
+}
+
+function verifyToken(token:string){
+    return jwt.verify(token,jwtSecret);
+}
+export {signInToken,verifyToken};
