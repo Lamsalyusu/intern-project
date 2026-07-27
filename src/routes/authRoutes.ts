874d87231705b4c15658ac175a91b794e0fd0090
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-27 11:14:01 +0545

    user routes

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router();
+
+import authController from '../controllers/authController';
+import authMiddleware from '../middlewares/authmiddleware';
+import validation from '../middlewares/validatemiddleware'
+import { registerSchema,loginSchema } from '../validators/authValidator';
+
+// import { registerSchema, loginSchema } from '../validators/authValidator';
+router.post('/register',validation(registerSchema),authController.register);
+router.post('/login',validation(loginSchema),authController.login);
+router.get('/me',authMiddleware,authController.me)
+
+export default router;
