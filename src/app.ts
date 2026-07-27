46149f049f5a4027e51313182d51fe6189dda3db
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-27 11:18:51 +0545

    db configuration

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import router from './routes/authRoutes';
+const app = express();
+app.use(express.json());
+import cors from 'cors';
+app.use(cors());
+
+
+app.use('/api/v1/auth',router);
+app.get('/health',(req,res)=>{
+    res.status(200).json({status:'ok',message:'banking server running smoothly'});
+});
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
-import sequelize from "./config/db";
-async function connectDB(){
-    try{
-        await sequelize.authenticate();
-        console.log("DB Connected successfully");
-    }
-    catch(error){
-        console.log("Error while connecting DB",error);
-    }
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+import  sequelize  from "./config/db";
+import app from './app';
+
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log("DB Connected successfully");
+  } catch (error) {
+    console.log("Error while connecting DB", error);
+  }
 }
+
 connectDB();
-const dotenv = require('dotenv');
-dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
