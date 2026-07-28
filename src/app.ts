3f47077706cc216abdc94ba76ad15b6893622b35
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-28 13:19:40 +0545

    routing for tasks in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes/authRoutes';
+import taskrouter from './routes/taskRoutes'
 const app = express();
 app.use(express.json());
 import cors from 'cors';
@@ -7,6 +8,7 @@ app.use(cors());
 
 
 app.use('/api/v1/auth',router);
+app.use('/api/v1/tasks',taskrouter)
 app.get('/health',(req,res)=>{
     res.status(200).json({status:'ok',message:'app running smoothly'});
 });
