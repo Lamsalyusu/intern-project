7e4289bf53a4d65dd62bce1861f40ae054386786
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-29 17:44:28 +0545

    task collaborator validation schema

--- src/validators/taskCollaboratorValidator.ts
+++ src/validators/taskCollaboratorValidator.ts
@@ -2,4 +2,4 @@ import {z} from 'zod';
 const collaboratorschema = z.object({
     email:z.string().email("invalid email format"),
 });
-export type taskcollaborator = z.infer<typeof collaboratorschema>;
+export type taskcollaboratorvalidation = z.infer<typeof collaboratorschema>;
