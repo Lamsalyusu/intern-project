a22467bc59575cc7e14e120e78afe381bd7e1276
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-29 17:48:03 +0545

    taskcollaboration repository

---
+++ src/repositories/taskCollaboratorRepository.ts
@@ -0,0 +1,22 @@
+import { TaskCollaborator } from "../models";
+
+
+//--> is this specific person a collaborator on this specific task 
+async function findOne(task_id:string,user_id:string){
+    return TaskCollaborator.findOne({where:{task_id,user_id}});
+}
+
+// --> who are the collaborators on this task
+async function findAllByTask(task_id:string){
+    return TaskCollaborator.findAll({where:{task_id}});
+}
+
+async function add(task_id:string,user_id:string){
+    return TaskCollaborator.create({user_id,task_id});
+}
+
+async function remove(task_id:string,user_id:string){
+    return TaskCollaborator.destroy({where:{task_id,user_id}})
+}
+
+export {findOne,findAllByTask,add,remove};
