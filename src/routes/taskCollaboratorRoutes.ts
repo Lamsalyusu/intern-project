f924113fabfab7eaad27858095ed374c129b81b4
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-30 10:13:24 +0545

    collbaoration routes

--- src/routes/taskCollaboratorRoutes.ts
+++ src/routes/taskCollaboratorRoutes.ts
@@ -1,4 +1,12 @@
 import express from 'express';
 const taskCollaboratorRouter = express.Router();
 import taskCollaboratorController from '../controllers/taskCollaboratorController';
-import {taskcollaboratorvalidation}  from '../validators/taskCollaboratorValidator';
+import { collaboratorschema } from '../validators/taskCollaboratorValidator';
+import { validation } from '../middlewares/validatemiddleware';
+import authMiddleware from '../middlewares/authmiddleware';
+
+taskCollaboratorRouter.post('/:id/collaborators', authMiddleware, validation(collaboratorschema), taskCollaboratorController.create);
+taskCollaboratorRouter.get('/:id/collaborators', authMiddleware, taskCollaboratorController.getCollaborator);
+taskCollaboratorRouter.delete('/:id/collaborators/:userId', authMiddleware, taskCollaboratorController.deleteCollaborators);
+
+export default taskCollaboratorRouter;
