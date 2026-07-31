87a0d1f09a4354f02be7345e0dc2934f8d4f9afb
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-07-31 17:24:25 +0545

    messages services got getting creating and checking the access of the messages

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,24 +3,18 @@ import { findByEmail, createUser } from "../repositories/userRepository";
 import { signInToken } from "../utils/jwt";
 import { RegisterInput, LoginInput } from "../validators/authValidator";
 
-
 async function registerUser(data: RegisterInput) {
   const existingUser = await findByEmail(data.email);
-
   if (existingUser) {
     throw { status: 409, message: "Email already in use" };
   }
-
   const password_hash = await bcrypt.hash(data.password,10);
-
   const user = await createUser({
     name: data.name,
     email: data.email,
     password_hash,
   }) as any;
-
   const token = signInToken({ id: user.id, email: user.email });
-
   return {
     user: { id: user.id, name: user.name, email: user.email, role: user.role },
     token,
@@ -33,15 +27,11 @@ async function loginUser(data: LoginInput) {
   if (!user) {
     throw { status: 401, message: "Invalid email or password" };
   }
-
   const isMatch = await bcrypt.compare(data.password, user.password_hash);
-
   if (!isMatch) {
     throw { status: 401, message: "Invalid email or password" };
   }
-
   const token = signInToken({ id: user.id, email: user.email });
-
   return {
     user: { id: user.id, name: user.name, email: user.email, role: user.role },
     token,
---
+++ src/services/messageService.ts
@@ -0,0 +1,37 @@
+import { findById as findTaskById } from "../repositories/taskRepository";
+import { findOne as findCollaborator } from "../repositories/taskCollaboratorRepository";
+import { createMessage, findByTask } from "../repositories/messageRepository";
+
+
+//yo function chai if owner and collborator lai access cha ki nai bhanne ko lagi 
+async function checkAccess(task_id:string,user_id:string){
+    const task = await findTaskById(task_id);
+    if(!task){
+        throw { status :403 ,message:'task not found'}
+    }
+    // yo condition ma chai task ko owner ho bhanne bujhinchha
+    const owner = task.owner_id === user_id;
+    // yo condition le chai aba collaborator ho ki nai bhanera check hunchha
+    const iscollaborator = await findCollaborator(task_id,user_id);
+    // if no owner and collaborator then no access to chat
+    if(!owner && !iscollaborator){
+        throw { status :403 ,message: 'not authorized to access the chat'}
+    }
+
+}
+
+// yo function chai message sending ko lagi 
+async function sendMessage(task_id:string,sender_id:string,body:string){
+    // yahan check hunxa yo taks ko access chha ki nai bhanera ani mathi ko condition ma check hunxa yo function bata
+    await checkAccess(task_id,sender_id);
+    // if access chha bhane yahan bata message create bhayo --> message garna milyo 
+    const message = await createMessage(task_id,sender_id,body)
+    return message;
+}
+
+async function getMessage(task_id:string,requesterId:string,page:number,limit:number){
+    await checkAccess(task_id,requesterId);
+    const message = await findByTask(task_id,page,limit);
+    return message;
+}
+export {sendMessage,getMessage}
