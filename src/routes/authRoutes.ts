5f8dd2f8ad2b3d4f0261643d51f7b0b92448d703
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-03 17:16:23 +0545

    feat: auth routes rate limiter

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 const router = express.Router();
-
+import limit from '../middlewares/rateLimiter';
 import authController from '../controllers/authController';
 import authMiddleware from '../middlewares/authmiddleware';
-import {validation,validateQuery} from '../middlewares/validatemiddleware'
+import {validation} from '../middlewares/validatemiddleware'
 import { registerSchema,loginSchema } from '../validators/authValidator';
 
 // import { registerSchema, loginSchema } from '../validators/authValidator';
-router.post('/register',validation(registerSchema),authController.register);
-router.post('/login',validation(loginSchema),authController.login);
+router.post('/register',limit,validation(registerSchema),authController.register);
+router.post('/login',limit,validation(loginSchema),authController.login);
 router.get('/me',authMiddleware,authController.me)
 
 export default router;
