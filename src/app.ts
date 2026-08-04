bcb6471d1f01948ec05d05633b7bfdb289d3b307
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-04 09:59:55 +0545

    feat(security): added helmet security

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,30 @@
 import express from 'express';
-import router from './routes/authRoutes';
-import taskrouter from './routes/taskRoutes'
-import errorHandler from './middlewares/errormiddleware';
-import helmet from 'helmet';
-const app = express();
-const helmes = helmet()
-app.use(helmet());
-app.use(express.json());
 import cors from 'cors';
+import helmet from 'helmet';
+import router from './routes/authRoutes';
+import taskrouter from './routes/taskRoutes';
 import taskCollaboratorRouter from './routes/taskCollaboratorRoutes';
 import messageRoutes from './routes/messageRoutes';
+import errorHandler from './middlewares/errormiddleware';
+
+const app = express();
+
+app.use(helmet()); 
+app.use(express.json());
 app.use(cors());
 
+// Routes
+app.use('/api/v1/auth', router);
+app.use('/api/v1/tasks', taskrouter);
+app.use('/api/v1/tasks', taskCollaboratorRouter);
+app.use('/api/v1/tasks', messageRoutes);
 
-app.use('/api/v1/auth',router);
-app.use('/api/v1/tasks',taskrouter)
-app.use('/api/v1/tasks',taskCollaboratorRouter)
-app.use('/api/v1/tasks',messageRoutes)
-app.get('/health',(req,res)=>{
-    res.status(200).json({status:'ok',message:'app running smoothly'});
+// Health check
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok', message: 'app running smoothly' });
 });
+
+// Error handling middleware 
 app.use(errorHandler);
+
 export default app;
