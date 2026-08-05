1933782b602de40ecb00b5c6a197841c3eace154
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-05 10:10:30 +0545

    fix: response status changed for message retreiving

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -9,7 +9,7 @@ const messageControllers = {
             const page = Number(req.query.page) || 1;
             const limit = Number(req.query.limit) || 10;
             const result = await getMessage(task_id,reqid,page,limit);
-            return res.status(201).json({data:result,message:'message retrived successfully'});
+            return res.status(200).json({data:result,message:'message retrived successfully'});
         }
         catch(error:any){
         next(error);
