27d42f2ed11a50c481cf6d6c8f151d407595999c
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-05 09:54:46 +0545

    fix: socket integration

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -6,9 +6,9 @@ function initSocket(httpServer:any){
 const io = new Server(httpServer,{
     cors:{
     origin:process.env.CLIENT_URL || 'http://0.0.0.0:3000',
-    methods:['GET','POST']
+    // methods:['GET','POST']
   }
-})
+});
   const chatNamespace: Namespace = io.of('/chat');
 
   chatNamespace.use(socketMiddleware);
