d74fcc026547bbc0f19f186ef5da71ba9f16d1ad
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-06 16:55:09 +0545

    fix: routing fixed along with proper validation

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -2,12 +2,12 @@ import express from 'express';
 const notifroutes = express.Router();
 import notificationController from '../controllers/notificationController';
 import authMiddleware from '../middlewares/authmiddleware';
-import { validation } from '../middlewares/validatemiddleware';
+import { validation,validateParams,validateQuery } from '../middlewares/validatemiddleware';
 import { notificationValidationSchema } from '../validators/notificationValidator';
 import { notificationIdSchema } from '../validators/notificationIdSchema';
 // notifroutes.post('/', authMiddleware, validation(notificationValidationSchema), notificationController.create);
-notifroutes.put('/:id/read', authMiddleware, validation(notificationIdSchema), notificationController.markAsRead);
-notifroutes.get('/', authMiddleware, validation(notificationValidationSchema), notificationController.getByUser);
-notifroutes.get('/unread/count', authMiddleware, notificationController.countUnread);
+notifroutes.put('/:id/read', authMiddleware, validateParams(notificationIdSchema), notificationController.markAsRead);
+notifroutes.get('/', authMiddleware, validateQuery(notificationValidationSchema), notificationController.getByUser);
+notifroutes.get('/unread/count', authMiddleware, validateQuery(notificationValidationSchema), notificationController.countUnread);
 
 export default notifroutes;
