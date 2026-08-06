a6f258ea2e6f55b84bec83fbb5f456eae979037c
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-06 17:18:54 +0545

    fix: migrations for notification

--- src/database/migrations/20260721110857-create-notifications.js
+++ src/database/migrations/20260721110857-create-notifications.js
@@ -43,6 +43,11 @@ module.exports = {
         allowNull: false,
         defaultValue: Sequelize.NOW,
       },
+      updated_at:{
+        type:Sequelize.DATE,
+        allowNull:false,
+        defaultValue:Sequelize.NOW,
+      }
     });
   },
 
