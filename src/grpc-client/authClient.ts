91234a04af33fe23ead89e605a9a6f510550bd2a
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-27 23:18:36 +0545

    fix: added auth client for fixing

--- src/grpc-client/authClient.ts
+++ src/grpc-client/authClient.ts
@@ -1,15 +1,9 @@
-import grpc from '@grpc/grpc-js';
-import protoloader from '@grpc/proto-loader'
+import * as grpc from '@grpc/grpc-js';
+import * as protoloader from '@grpc/proto-loader'
 import ENV from "../../../server/src/config/env";
-
 const packageDefinition = protoloader.loadSync(ENV.AUTH_PROTO_PATH, ENV.PROTO_LOADER_OPTIONS,);
 const authProto = grpc.loadPackageDefinition(packageDefinition) as any;
 const AuthService = authProto.authPackage.auth;
 const AUTH_HOST_URL = process.env.PORT || 'localhost:50051';
-
-const AuthClient = new AuthService(
-    AUTH_HOST_URL,
-    grpc.credentials.createInsecure()
-);
-
+const AuthClient = new AuthService( AUTH_HOST_URL, grpc.credentials.createInsecure());
 export default AuthClient;
