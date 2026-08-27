cfa02be8184c873fd280f34c5edf8b65f61c4d01
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-27 23:17:30 +0545

    fix: fixed auth route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,8 +7,8 @@ import {validation} from '../middlewares/validatemiddleware'
 import { registerSchema,loginSchema } from '../validators/authValidator';
 
 // import { registerSchema, loginSchema } from '../validators/authValidator';
-router.post('/register',limit,validation(registerSchema),authController.register);
-router.post('/login',limit,validation(loginSchema),authController.login);
-router.get('/me',authMiddleware,authController.me)
+router.post('/register',validation(registerSchema),authController.register);
+router.post('/login',validation(loginSchema),authController.login);
+// router.get('/me',authMiddleware,authController.me)
 
 export default router;
